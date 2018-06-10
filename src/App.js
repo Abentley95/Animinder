2c6dbf38dc9a2fb3f1568dfea3e3ebb30ccb36e8
Austin Bentley <austinbentley@Austins-MacBook-Pro.local>
2018-06-10 12:32:57 -0600

    boilerplate cleanup/electron setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Welcome = styled.h1`
+    font-size: 150px;
+`;
+
+const App = () =>  <Welcome>hello</Welcome>
+
+export default App;
